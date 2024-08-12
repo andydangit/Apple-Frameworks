9334a72daa2463dfa71d6983373ceb89f650c961
andydangit <andy.dang.it@gmail.com>
2024-08-12 06:30:55 -0500

    Add: new Framewor Grid View file

---
+++ Apple-Frameworks/FrameworkGridView.swift
@@ -0,0 +1,18 @@
+//
+//  FrameworkGridView.swift
+//  Apple-Frameworks
+//
+//  Created by FartFire on 8/12/24.
+//
+
+import SwiftUI
+
+struct FrameworkGridView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    FrameworkGridView()
+}
