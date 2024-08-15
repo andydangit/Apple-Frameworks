19ed34ba5e475525fd8e905d4f1fa5158252c4c2
andydangit <andy.dang.it@gmail.com>
2024-08-15 14:27:42 -0500

    Add: design view for button and move body to center

--- Apple-Frameworks/FrameworkDetailView.swift
+++ Apple-Frameworks/FrameworkDetailView.swift
@@ -13,17 +13,31 @@ struct FrameworkDetailView: View {
     
     var body: some View {
         VStack {
+            Spacer()
+            
             FrameworkTitleView(framework: framework)
+            Text(framework.description)
+                .font(.body)
+                .padding()
+            
+            Spacer()
+            
+            Button{
+                
+            }label: {
+                Text("Learn More")
+                    .font(.title2)
+                    .fontWeight(.semibold)
+                    .frame(width: 280, height: 50)
+                    .background(Color.red)
+                    .foregroundColor(.white)
+                    .cornerRadius(10)
+            }
         }
     }
 }
 
 #Preview {
-    FrameworkDetailView(framework: Framework(
-        name: "ARKit",
-                  imageName: "arkit",
-                  urlString: "https://developer.apple.com/augmented-reality/arkit",
-                  description: "ARKit 4 introduces a brand-new Depth API, creating a new way to access the detailed depth information gathered by the LiDAR Scanner on iPhone 12 Pro, iPhone 12 Pro Max, and iPad Pro.\n\nLocation Anchors leverages the higher-resolution data in Apple Maps to place AR experiences at a specific point in the world in your iPhone and iPad apps. And support for face tracking extends to all devices with the Apple Neural Engine and a front-facing camera, so even more users can experience the joy of AR in photos and videos.")
-    )
+    FrameworkDetailView(framework: MockData.sampleFramework)
         .preferredColorScheme(.dark)
 }
