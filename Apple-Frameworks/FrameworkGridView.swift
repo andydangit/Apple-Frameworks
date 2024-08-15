63ff2aa53091c8ecacd10e968d7df88ced6268a5
andydangit <andy.dang.it@gmail.com>
2024-08-15 13:00:38 -0500

    Added: LazyVGrid and columns as griditem

--- Apple-Frameworks/FrameworkGridView.swift
+++ Apple-Frameworks/FrameworkGridView.swift
@@ -8,9 +8,25 @@
 import SwiftUI
 
 struct FrameworkGridView: View {
+    
+    let columns: [GridItem] = [GridItem(.flexible()),
+                               GridItem(.flexible()),
+                               GridItem(.flexible())
+    
+    ]
+    
+    
+    
     var body: some View {
-        FrameworkTitleView(name: "App clips", imageName: "app-clip")
+        LazyVGrid(columns: columns) {
+            FrameworkTitleView(name: "App clips", imageName: "app-clip")
+            FrameworkTitleView(name: "App clips", imageName: "app-clip")
+            FrameworkTitleView(name: "App clips", imageName: "app-clip")
+            FrameworkTitleView(name: "App clips", imageName: "app-clip")
+            FrameworkTitleView(name: "App clips", imageName: "app-clip")
+            FrameworkTitleView(name: "App clips", imageName: "app-clip")
         }
+    }
 }
 
 #Preview {
