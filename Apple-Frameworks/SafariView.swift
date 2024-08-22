5936df83300e51b3a3b4546ef3f11614b8cd792e
andydangit <andy.dang.it@gmail.com>
2024-08-22 16:33:49 -0500

    Add: SafriView Page

---
+++ Apple-Frameworks/SafariView.swift
@@ -0,0 +1,22 @@
+//
+//  SafariView.swift
+//  Apple-Frameworks
+//
+//  Created by DangIt on 8/22/24.
+//
+
+import SwiftUI
+import SafariServices
+
+struct SafariView: UIViewControllerRepresentable {
+    
+    let url: URL
+    
+    func makeUIViewController(context: UIViewControllerRepresentableContext<SafariView>) -> SFSafariViewController {
+        SFSafariViewController(url: url)
+    }
+    
+    func updateUIViewController(_ uiViewController: UIViewControllerType, context: Context) {
+        <#code#>
+    }
+}
