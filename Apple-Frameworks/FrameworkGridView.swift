6ea41ff0db4fa8666e5995bea0e7f7007bb5d9ce
andydangit <andy.dang.it@gmail.com>
2024-08-23 13:02:46 -0500

    Refactor: created a titleView page and moved the FrameworkTitleView into it

--- Apple-Frameworks/FrameworkGridView.swift
+++ Apple-Frameworks/FrameworkGridView.swift
@@ -14,7 +14,7 @@ struct FrameworkGridView: View {
     var body: some View {
         NavigationView {
             ScrollView{
-                LazyVGrid(columns: viewModel.columns) { 
+                LazyVGrid(columns: viewModel.columns) {
                     ForEach(MockData.frameworks) { framework in
                         FrameworkTitleView(framework: framework)
                             .onTapGesture {
@@ -37,21 +37,3 @@ struct FrameworkGridView: View {
         .preferredColorScheme(.dark)
 }
 
-struct FrameworkTitleView: View {
-    let framework: Framework
-    
-    var body: some View {
-        VStack{
-            Image(framework.imageName)
-                .resizable()
-                .frame(width: 90, height: 90)
-            Text(framework.name)
-                .font(.title2)
-                .fontWeight(.semibold)
-                .scaledToFit()
-                .minimumScaleFactor(0.6)
-        }
-        .padding()
-    }
-    
-}
